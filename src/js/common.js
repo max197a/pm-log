fa7d55517ca407613be6980d0c6ae917e87e195d
max197a <max197aa@gmail.com>
2018-02-20 12:59:13 +0200

    bus page

--- src/js/common.js
+++ src/js/common.js
@@ -38,51 +38,89 @@ $(document).ready(function() {
 
 
     $('.our-customers__carousel').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      nav: true
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nav: true
     });
     $('.transport__carousel').slick({
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      nav: false,
-      dots: false,
-      responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              nav: false,
-              loop: false,
-              dots: true
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nav: false,
+        dots: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    nav: false,
+                    loop: false,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    nav: false,
+                    loop: false,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    nav: false,
+                    loop: false,
+                    dots: true,
+                    slidesToScroll: 1
+                }
             }
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
-              nav: false,
-              loop: false,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              nav: false,
-              loop: false,
-              dots: true,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
         ]
     });
+
+    $('.bus-head-left').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        asNavFor: '.bus-body-left'
+    });
+    $('.bus-body-left').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.bus-head-left',
+        dots: false,
+        fade: true,
+        draggable: false,
+        arrows: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
+    $('.bus-head-right').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        asNavFor: '.bus-body-right'
+    });
+    $('.bus-body-right').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.bus-head-right',
+        dots: false,
+        fade: true,
+        draggable: false,
+        arrows: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
 
     $(document).on('click', 'close-popup', function() {
         $.magnificPopup.close();
